b1b7c358e4f36bd07ec41804f52dab442d8c6171
yhya <yhyasallom@gmail.com>
2019-09-01 03:23:55 +0300

    improve

--- coursesapp/public/index.php
+++ coursesapp/public/index.php
@@ -1,29 +1,6 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-require '../vendor/autoload.php';
-
-$app = new \Slim\App([
-'settings'=>[
-
-    'displayErrorDetails'=>true
-]
-
-]);
-
-$app->GET('/Hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
-$app->POST('/Reg/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("done!");
-    return $response;
-});
+require __DIR__ . '/../bootstrap/app.php';
 
 
 $app->run();
